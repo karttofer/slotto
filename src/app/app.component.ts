25f4ec128a3bee0dde61eb2a0778ebcb575f57ed
karttofer <jhornancolina@gmail.com>
2024-01-15 17:17:44 -0500

    delete no necessary files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-// Components
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
