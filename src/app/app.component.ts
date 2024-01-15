188daad911798efbedbae75d6f7c008679ada48f
karttofer <jhornancolina@gmail.com>
2024-01-15 17:14:37 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
+// Dependencies
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+// Components
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
