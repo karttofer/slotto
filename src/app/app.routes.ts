f992aca6ee0cdd3264845ffc5c5cd37ccaca28e2
karttofer <jhornancolina@gmail.com>
2024-01-15 19:00:23 -0500

    add sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+// Dependencies
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Components
+import { ChatComponent } from './sections/chat/chat.component';
+import { LoginComponent } from './sections/login/login.component';
+import { SignUpComponent } from './sections/sign-up/sign-up.component';
+
+export const routes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'login', component: LoginComponent }
+];
